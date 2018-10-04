abbb844204119a825cbac041577e71af548f6c7a
Giovanni Papini <gpapo95@gmail.com>
2018-10-04 11:57:00 +0200

    Add 1st point of 1st exercise of 3rd week

---
+++ julia-scripts/week-03.jl
@@ -0,0 +1,9 @@
+
+using SpecialFunctions
+using Plots
+
+B(a, b) = gamma(a) * gamma(b) / gamma(a + b)
+pdf_ψ(x) = 1 / B(1, 1) * exp(x) / (1 + exp(x))^2
+
+plot(pdf_ψ, legend = false)
+savefig("julia-scripts/week-03_plot1.png")
